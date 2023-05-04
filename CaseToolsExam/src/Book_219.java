f6785350c7a9371c3f2e1294000629e03491c436
raynell08 <122138273+raynell08@users.noreply.github.com>
2023-05-04 09:32:09 +0530

    Update Book_219.java

--- CaseToolsExam/src/Book_219.java
+++ CaseToolsExam/src/Book_219.java
@@ -9,6 +9,11 @@ public class Book_219 {
 		this.total_pages = total_pages;
 		this.page_num = 1;
 	}
+	int go_forward()
+	{
+		page_num++;
+		return page_num;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
