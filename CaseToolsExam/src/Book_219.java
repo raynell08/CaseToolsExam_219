7dfcf886af875a75126a0efcf87cd152cba51c30
exam <exam@Bcalab4-m18>
2023-05-04 09:17:58 +0530

    this is the first git

---
+++ CaseToolsExam/src/Book_219.java
@@ -0,0 +1,18 @@
+
+public class Book_219 {
+	
+	int page_num;
+	int total_pages;
+	
+	Book_219(int total_pages)
+	{
+		this.total_pages = total_pages;
+		this.page_num = 1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
